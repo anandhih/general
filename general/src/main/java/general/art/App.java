0a96d12e9e44494c8474b9a805d9193d060a4b0d
anandhi haridass <anandhi.haridass@otto.de>
2018-05-24 13:09:02 +0200

    initial general commit.

---
+++ general/src/main/java/general/art/App.java
@@ -0,0 +1,13 @@
+package general.art;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
